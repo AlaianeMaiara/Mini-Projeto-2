3c19db366ed86bdca10fa461a2175c314996673f
ALAIANE MAIARA FERREIRA DOS SANTOS <santoslala880@gmail.com>
2026-09-14 11:23:11 -0300

    feat: adiciona array fixo de livros

---
+++ checkout-react/src/data/produtos.js
@@ -0,0 +1,30 @@
+export const produtos = [
+  {
+    id: 1,
+    nome: "Algoritmos",
+    autor: "Thomas H. Cormen",
+    preco: 59.90,
+    quantidade: 1,
+  },
+  {
+    id: 2,
+    nome: "SQL",
+    autor: "Alan Beaulieu",
+    preco: 49.90,
+    quantidade: 1,
+  },
+  {
+    id: 3,
+    nome: "Os Elefantes Não Esquecem",
+    autor: "Agatha Christie",
+    preco: 39.90,
+    quantidade: 1,
+  },
+  {
+    id: 4,
+    nome: "Essencialismo",
+    autor: "Greg McKeown",
+    preco: 44.90,
+    quantidade: 1,
+  },
+];
